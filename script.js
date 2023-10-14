bb7596fc5ba7229dce5b0d39a908cb2b9ab2d0a2
Vladislav <vladislav.work98@gmail.com>
2023-10-14 12:01:58 +0300

    I wrote a script that performs arithmetic operations of addition, subtraction, multiplication, division and division with remainder.
    Added an opportunity to string together several operations and get the right answer, with each pair of numbers being evaluated at a time.

--- script.js
+++ script.js
@@ -1,6 +1,61 @@
-let firstNumber;
-let secondNumber;
+const numbersButtons = document.querySelectorAll('.numbers');
+const display = document.querySelector('.input');
+const arithmeticOperatorBtns = document.querySelectorAll('.arithmeticOperator');
+const equalSign = document.querySelector('.equal');
+
+let firstNumber = 0;
+let secondNumber = 0;
 let operator; 
+let displayValue = 0;
+let resultValue = 0;
+
+numbersButtons.forEach(btn => btn.addEventListener('click', displayNumber))
+arithmeticOperatorBtns.forEach(btn => btn.addEventListener('click', e => {
+    if (secondNumber === 0) {
+        operator = e.target.textContent;
+        return;
+    }
+
+    switch (operator) {
+        case '+': 
+            resultValue = add(firstNumber, secondNumber);
+            display.textContent = resultValue;
+            firstNumber = resultValue;
+            secondNumber = 0;
+            operator = operator = e.target.textContent;
+            break;
+        case '-': 
+            resultValue = subtract(firstNumber, secondNumber);
+            display.textContent = resultValue;
+            firstNumber = resultValue;
+            secondNumber = 0;
+            operator = operator = e.target.textContent;
+            break;
+        case '*': 
+            resultValue = multiply(firstNumber, secondNumber);
+            display.textContent = resultValue;
+            firstNumber = resultValue;
+            secondNumber = 0;
+            operator = operator = e.target.textContent;
+            break;
+        case '/': 
+            resultValue = divide(firstNumber, secondNumber);
+            display.textContent = resultValue;
+            firstNumber = resultValue;
+            secondNumber = 0;
+            operator = operator = e.target.textContent;
+            break;
+        case '%': 
+            resultValue = divideWithRemainder(firstNumber, secondNumber);
+            display.textContent = resultValue;
+            firstNumber = resultValue;
+            secondNumber = 0;
+            operator = operator = e.target.textContent;
+            break;
+    }
+    
+}));
+equalSign.addEventListener('click', () => operate(firstNumber, secondNumber, operator))
 
 function add(a, b) {
     return a + b;
@@ -18,15 +73,72 @@ function divide(a, b) {
     return a / b;
 }
 
+function divideWithRemainder(a, b) {
+    return a % b;
+}
+
 function operate(firstNumber, secondNumber, operator) {
     switch (operator) {
         case '+':
-            return add(firstNumber, secondNumber);
+            if (secondNumber === 0) return;
+           
+            display.textContent = add(firstNumber, secondNumber);
+            
+            break;
         case '-':
-            return subtract(firstNumber, secondNumber);
+            if (secondNumber === 0) return;
+
+            display.textContent = subtract(firstNumber, secondNumber);
+            
+            break;
         case '*': 
-            return multiply(firstNumber, secondNumber);
+            if (secondNumber === 0) return;
+
+            display.textContent = multiply(firstNumber, secondNumber);
+            
+            break;
         case '/':
-            return divide(firstNumber, secondNumber);
+            if (secondNumber === 0) return;
+
+            display.textContent = divide(firstNumber, secondNumber);
+            
+            break;
+        case '%':
+            if (secondNumber === 0) return;
+
+            display.textContent = divideWithRemainder(firstNumber, secondNumber);
+            
+            break;
     }
 }
+
+function displayNumber(e) {
+    if (operator === undefined) {
+
+        if (firstNumber === 0) {
+            displayValue = e.target.textContent;
+
+            firstNumber = +displayValue;
+        } else {
+            displayValue += e.target.textContent;
+
+            firstNumber = +displayValue;
+        }
+    
+        display.textContent = displayValue;
+    } else {
+        
+        if (secondNumber === 0) {
+            displayValue = e.target.textContent;
+
+            secondNumber = +displayValue;
+        } else {
+            displayValue += e.target.textContent;
+
+            secondNumber = +displayValue;
+        }
+    
+        display.textContent = displayValue;
+    }
+}
+
