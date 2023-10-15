1697662336b66ad877778d87c6f112489230bbf9
Vladislav <vladislav.work98@gmail.com>
2023-10-15 20:54:31 +0300

    removed the condition and changed the parameter of the functions calculate and showNumberOnDisplay. changed the condition for launching the calculate function when the sign is pressed

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ let resultValue = false;
 let operator = null; 
 let displayValue;
 
-numbersButtons.forEach(btn => btn.addEventListener('click', e => showNumberOnDisplay(e, firstNumber, secondNumber, operator)));
-arithmeticOperatorBtns.forEach(btn => btn.addEventListener('click', e => calculate(e, firstNumber, secondNumber, operator)));
+numbersButtons.forEach(btn => btn.addEventListener('click', e => showNumberOnDisplay(e.target.textContent, firstNumber, secondNumber, operator)));
+arithmeticOperatorBtns.forEach(btn => btn.addEventListener('click', e => calculate(e.target.textContent, firstNumber, secondNumber, operator)));
 equalSign.addEventListener('click', () => operate(firstNumber, secondNumber, operator));
 allClearBtn.addEventListener('click', allClear);
 clearBtn.addEventListener('click', clear);
@@ -23,7 +23,7 @@ document.addEventListener('keydown', e => {
 })
 document.addEventListener('keypress', e => {
     if (/[0-9.]/.test(e.key)) {
-        showNumberOnDisplay(e, firstNumber, secondNumber, operator)
+        showNumberOnDisplay(e.key, firstNumber, secondNumber, operator)
     }
 
     if (e.key === '=' || e.key === 'Enter') {
@@ -31,7 +31,7 @@ document.addEventListener('keypress', e => {
         return;
     }
 
-    if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/' || e.key === '%') calculate(e, firstNumber, secondNumber, operator);
+    if (/[+\\-\\*\\/\\%]/.test(e.key)) calculate(e.key, firstNumber, secondNumber, operator);
 
 })
 
@@ -55,210 +55,107 @@ function divideWithRemainder(a, b) {
     return a % b;
 }
 
-function showNumberOnDisplay(e, firstOperand, secondOperand, mathOperator) {
-    if (e.key === undefined) {
-        if (e.target.textContent === '.' && resultValue || e.target.textContent === '.' && firstOperand === null ) return;
-        if (displayValue?.toString().includes('.') && e.target.textContent === '.') return;
+function showNumberOnDisplay(character, firstOperand, secondOperand, mathOperator) {
+    if (character === '.' && resultValue || character === '.' && firstOperand === null ) return;
+    if (displayValue?.toString().includes('.') && character === '.') return;
+
+    if (!mathOperator && resultValue) {
+        mathOperator = null;
+        firstOperand = null;
+        resultValue = false;
+    };
     
-        if (!mathOperator && resultValue) {
-            mathOperator = null;
-            firstOperand = null;
-            resultValue = false;
-        };
+    if (mathOperator === null || firstOperand === 0) {
+        if (firstOperand === null || (displayValue === '0' && character !== '.') ) { 
+            
+            displayValue = character;
+            firstNumber = +displayValue;
+        } else { 
+            
+            if (displayValue.length > 8) return;
         
-        if (mathOperator === null || firstOperand === 0) {
-            if (firstOperand === null || (displayValue === '0' && e.target.textContent !== '.') ) { 
-                
-                displayValue = e.target.textContent;
-                firstNumber = +displayValue;
-            } else { 
-                
-                if (displayValue.length > 8) return;
-            
-                displayValue += e.target.textContent;
-                firstNumber = +displayValue;
-            }
-            
-            display.textContent = displayValue;
-        } else {
-            
-            if (secondOperand === null) {
-                
-                displayValue = e.target.textContent;
-
-                secondNumber = +displayValue;
-            } else {
-
-                if (displayValue.length > 8) return;
-                displayValue += e.target.textContent;
-
-                secondNumber = +displayValue;
-            }
-        
-            display.textContent = displayValue;
+            displayValue += character;
+            firstNumber = +displayValue;
         }
+        
+        display.textContent = displayValue;
     } else {
-        if (e.key === '.' && resultValue || e.key === '.' && firstOperand === null ) return;
-        if (displayValue?.toString().includes('.') && e.key === '.') return;
-    
-        if (!mathOperator && resultValue) {
-            mathOperator = null;
-            firstOperand = null;
-            resultValue = false;
-        };
         
-        if (mathOperator === null || firstOperand === 0) {
-            if (firstOperand === null || (displayValue === '0' && e.key !== '.') ) { 
-                
-                displayValue = e.key;
-                firstNumber = +displayValue;
-            } else { 
-                
-                if (displayValue.length > 8) return;
+        if (secondOperand === null) {
             
-                displayValue += e.key;
-                firstNumber = +displayValue;
-            }
-            
-            display.textContent = displayValue;
+            displayValue = character;
+
+            secondNumber = +displayValue;
         } else {
-            
-            if (secondOperand === null) {
-                
-                displayValue = e.key;
 
-                secondNumber = +displayValue;
-            } else {
+            if (displayValue.length > 8) return;
+            displayValue += character;
 
-                if (displayValue.length > 8) return;
-                displayValue += e.key;
-
-                secondNumber = +displayValue;
-            }
-        
-            display.textContent = displayValue;
+            secondNumber = +displayValue;
         }
+    
+        display.textContent = displayValue;
     }
 }
 
-function calculate(e, firstOperand, secondOperand, mathOperator) {
+function calculate(sign, firstOperand, secondOperand, mathOperator) {
     if (firstOperand === null) return;
-    if (e.key === undefined) {
-        if (secondOperand === null) {
-            operator = e.target.textContent;
-            return;
-        }
-        
-        switch (mathOperator) {
-            case '+': 
-                displayValue = add(firstOperand, secondOperand);
-                if (displayValue.toString().length > 9) {
-                    displayValue = displayValue.toString().slice(0, 9)
-                }
-                display.textContent = displayValue;
-                firstNumber = +displayValue;
-                secondNumber = null;
-                operator = e.target.textContent;
-                break;
-            case '-': 
-                displayValue = subtract(firstOperand, secondOperand);
-                if (displayValue.toString().length > 9) {
-                    displayValue = displayValue.toString().slice(0, 9)
-                }
-                display.textContent = displayValue;
-                firstNumber = +displayValue;
-                secondNumber = null;
-                operator = operator = e.target.textContent;
-                break;
-            case '*': 
-                displayValue = multiply(firstOperand, secondOperand);
-                if (displayValue.toString().length > 9) {
-                    displayValue = displayValue.toString().slice(0, 9)
-                }
-                display.textContent = displayValue;
-                firstNumber = +displayValue;
-                secondNumber = null;
-                operator = operator = e.target.textContent;
-                break;
-            case '/':
-                displayValue = divide(firstOperand, secondOperand);
-                if (displayValue.toString().length > 9) {
-                    displayValue = displayValue.toString().slice(0, 10)
-                }
-                display.textContent = displayValue;
-                firstNumber = +displayValue;
-                secondNumber = null;
-                operator = operator = e.target.textContent;
-                break;
-            case '%': 
-                displayValue = divideWithRemainder(firstOperand, secondOperand);
-                if (displayValue.toString().length > 9) {
-                    displayValue = displayValue.toString().slice(0, 9)
-                }
-                display.textContent = displayValue;
-                firstNumber = +displayValue;
-                secondNumber = null;
-                operator = operator = e.target.textContent;
-                break;
-        }
-    } else {
-        if (secondOperand === null) {
-            operator = e.key;
-            return;
-        }
-        
-        switch (mathOperator) {
-            case '+': 
-                displayValue = add(firstOperand, secondOperand);
-                if (displayValue.toString().length > 9) {
-                    displayValue = displayValue.toString().slice(0, 9)
-                }
-                display.textContent = displayValue;
-                firstNumber = +displayValue;
-                secondNumber = null;
-                operator = e.key;
-                break;
-            case '-': 
-                displayValue = subtract(firstOperand, secondOperand);
-                if (displayValue.toString().length > 9) {
-                    displayValue = displayValue.toString().slice(0, 9)
-                }
-                display.textContent = displayValue;
-                firstNumber = +displayValue;
-                secondNumber = null;
-                operator = e.key;
-                break;
-            case '*': 
-                displayValue = multiply(firstOperand, secondOperand);
-                if (displayValue.toString().length > 9) {
-                    displayValue = displayValue.toString().slice(0, 9)
-                }
-                display.textContent = displayValue;
-                firstNumber = +displayValue;
-                secondNumber = null;
-                operator = e.key;
-                break;
-            case '/':
-                displayValue = divide(firstOperand, secondOperand);
-                if (displayValue.toString().length > 9) {
-                    displayValue = displayValue.toString().slice(0, 10)
-                }
-                display.textContent = displayValue;
-                firstNumber = +displayValue;
-                secondNumber = null;
-                operator = e.key;
-                break;
-            case '%': 
-                displayValue = divideWithRemainder(firstOperand, secondOperand);
-                if (displayValue.toString().length > 9) {
-                    displayValue = displayValue.toString().slice(0, 9)
-                }
-                display.textContent = displayValue;
-                firstNumber = +displayValue;
-                secondNumber = null;
-                operator = e.key;
-                break;
-        }
+    if (secondOperand === null) {
+        operator = sign;
+        return;
+    }
+    
+    switch (mathOperator) {
+        case '+': 
+            displayValue = add(firstOperand, secondOperand);
+            if (displayValue.toString().length > 9) {
+                displayValue = displayValue.toString().slice(0, 9)
+            }
+            display.textContent = displayValue;
+            firstNumber = +displayValue;
+            secondNumber = null;
+            operator = sign;
+            break;
+        case '-': 
+            displayValue = subtract(firstOperand, secondOperand);
+            if (displayValue.toString().length > 9) {
+                displayValue = displayValue.toString().slice(0, 9)
+            }
+            display.textContent = displayValue;
+            firstNumber = +displayValue;
+            secondNumber = null;
+            operator = operator = sign;
+            break;
+        case '*': 
+            displayValue = multiply(firstOperand, secondOperand);
+            if (displayValue.toString().length > 9) {
+                displayValue = displayValue.toString().slice(0, 9)
+            }
+            display.textContent = displayValue;
+            firstNumber = +displayValue;
+            secondNumber = null;
+            operator = operator = sign;
+            break;
+        case '/':
+            displayValue = divide(firstOperand, secondOperand);
+            if (displayValue.toString().length > 9) {
+                displayValue = displayValue.toString().slice(0, 10)
+            }
+            display.textContent = displayValue;
+            firstNumber = +displayValue;
+            secondNumber = null;
+            operator = operator = sign;
+            break;
+        case '%': 
+            displayValue = divideWithRemainder(firstOperand, secondOperand);
+            if (displayValue.toString().length > 9) {
+                displayValue = displayValue.toString().slice(0, 9)
+            }
+            display.textContent = displayValue;
+            firstNumber = +displayValue;
+            secondNumber = null;
+            operator = operator = sign;
+            break;
     }
 }
 
